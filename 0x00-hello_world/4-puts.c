f683f37471166a9775611400970128de5cdfecd1
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-09 07:23:54 -0800

    co

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 
 int main(void)
 {
-	char str_var[] = "\"Programming is like building a multilingual puzzle";
-	puts(str_var, "\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
