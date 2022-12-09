f089a2e43fd92a6156bd090b8e5b24f26d13f008
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-09 07:59:31 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* using puts function to print */
+/**
+ *main: using puts function to print
+ *Return: 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
