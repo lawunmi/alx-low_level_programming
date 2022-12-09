d4e613e559b9a5653b53e39e04c3fcea95595378
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-09 08:13:57 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Prints a message using puts function
+ *
+ * Return: Always 0(SUccess)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
