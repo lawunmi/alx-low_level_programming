597abb19a411bdfb7e9ba4030bd4060b3956bfa4
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-10 13:04:08 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,6 +6,18 @@
  */
 int main(void)
 {
+	char c1;
+	char c2;
 
+	for (c1 = 'a'; c1 <= 'z'; c1++)
+	{
+		putchar(c1);
+	}
+	for (c2 = 'A'; c2 <= 'Z': c2++)
+	{
+		putchar(c2);
+	}
+	putchar('\n');
+	return (0);
 }
 
