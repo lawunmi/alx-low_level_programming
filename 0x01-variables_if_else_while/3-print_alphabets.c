2c94d1aef8d7a69cb0e8d2517fb2e6b0077ad4bf
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-09 16:45:24 -0800

    3-print_alphabets.c commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - upper and lower case alphabet
+ * Return - back to 0
+ *
+ */
+int main(void)
+{
+	putchar("abcdefghijklmnopqrstuvwxyz");
+	putchar("ABCDEFGHIJKLMNOPQRSTUVWXYZ\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This is a directory on variables and conditions.
