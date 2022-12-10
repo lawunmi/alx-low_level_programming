5a1f6d6b6a5ba811bc0b4319cccbc784c898b29f
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-10 11:00:55 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,6 +21,6 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 	else if (lastDigit < 6 && lastDigit != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 
-			lastDigit);
+		lastDigit);
 	return (0);
 }
