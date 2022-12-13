747625dbd2b9192942109a0bfbbad1dea726e29f
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-13 08:23:41 -0800

    1-alphabet.c commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,16 @@
  */
 int main(void)
 {
-	char c;
+	print_alphabet();
+	return (0);
+}
 
+void print_alphabet(void)
+{
+	char c;
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
 	putchar('\n');
-	return (0);
 }
