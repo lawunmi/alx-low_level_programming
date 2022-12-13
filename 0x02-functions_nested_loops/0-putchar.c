4091770228c83a1f746f6a41078a7c508b440bbc
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-13 06:32:32 -0800

    0-putchar.c commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * main - program that prints _putchar, followed by a new line
- * Return : 0
+ * Return: 0
  *
  */
 int main(void)
 {
 	char word[10] = "_putchar";
+
 	printf("%s\n", word);
 
 	return (0);
