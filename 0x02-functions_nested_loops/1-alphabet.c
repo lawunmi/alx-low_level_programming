324e4f6ca2edb2ee7b7a7935c64ef1e33eaada40
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-14 07:38:32 -0800

    1-alphabet.c commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_alphabet - function that prints the alphabet, in lowercase, then a new  * line.
+ * print_alphabet - function that prints the alphabet, in lowercase, then a new
+ * line
  *
  */
 void print_alphabet(void)
 {
 	char c;
-			
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
