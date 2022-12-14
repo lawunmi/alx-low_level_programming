cbd96e830845d395c986ce81d76eb006379f83cf
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-14 14:31:12 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int i, res;
+
+	for (i = 0; i <= 12; i++)
+	{
+		res = 9 * i;
+	}
+	_putchar(res);
+}
