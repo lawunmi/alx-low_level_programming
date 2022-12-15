1823cefc6606acb1dd37bc19b8fd69b1d05d2fe5
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-14 16:10:39 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * main - first 50 Fibonacci numbers, starting with 1 and 
+ * main - first 50 Fibonacci numbers, starting with 1 and
  * followed by a new line
  * Return : 0
- *
  */
 int main(void)
 {
 	long a, b, cnt, res;
+	a = 0;
+	b = 1;
 
 	for (cnt = 0; cnt < 50; cnt++)
 	{
