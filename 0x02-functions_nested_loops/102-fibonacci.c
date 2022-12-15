87d4c153d425ca490422cfb024b96f8ed9c41095
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-14 16:06:29 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - first 50 Fibonacci numbers, starting with 1 and 
+ * followed by a new line
+ * Return : 0
+ *
+ */
+int main(void)
+{
+	long a, b, cnt, res;
+
+	for (cnt = 0; cnt < 50; cnt++)
+	{
+		res = a + b;
+		a = b;
+		b = res;
+		printf("%lu", res);
+
+		if (cnt == 49)
+		{
+			putchar('\n');
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
