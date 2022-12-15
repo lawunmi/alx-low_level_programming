67e1beaf01613f620127b7b83b1060ea83aed781
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-14 17:10:13 -0800

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Fibonacci sequence whose values do not exceed 4000000
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	long a, b, cnt, res, ans;
+	a = 0;
+	b = 1;
+
+	for (cnt = 0; cnt < 50; cnt++)
+	{
+		res = a + b;
+		a = b;
+		b = res;
+
+		if (res < 4000000 && (res % 2 == 0))
+		{
+			ans += res;
+		}
+
+	}
+	printf("%ld\n", ans);
+	return (0);
+}
