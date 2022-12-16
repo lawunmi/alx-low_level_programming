d389d48eb623550cef6cc26a094616cc5a427080
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-16 05:10:39 -0800

    8-print_square.c commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line.
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(" ");
+		_putchar('\n');
+
+		for (j = 0; j < n; j++)
+		{
+			_putchar('#');
+		}
+	}
+	_putchar('\n');
+}
