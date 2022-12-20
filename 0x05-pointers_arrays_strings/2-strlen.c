61a18254206f2550e971ac5d94e26dca95821c7c
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-20 04:20:07 -0800

    2-strlen.c commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * s: pointer to be test
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+	int cnt = 0;
+	int len = strlen(s);
+
+	for(i = 0; i < len; i++)
+	{
+		if(isalpha(s[i]))
+			cnt++;
+	}
+	return cnt;
+}
