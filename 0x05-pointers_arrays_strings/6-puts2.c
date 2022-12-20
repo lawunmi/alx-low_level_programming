c51e19c9d6be378113485fde9269e7b0a47514ed
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-20 06:34:53 -0800

    6-puts2.c commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - prints every other character of a string, starting with the first
+ * @str: arguement to be passed
+ */
+
+void puts2(char *str)
+{
+	int i, len = strlen(str);
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
