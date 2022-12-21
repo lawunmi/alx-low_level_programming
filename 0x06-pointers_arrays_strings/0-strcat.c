a56d6e77aa367621c834a7f74f854566d46e7e70
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-21 06:06:50 -0800

    0-strcat.c commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strcat - function that concatenates two strings.
+ * @dest: first string
+ * @src: second string
+ * Return: concatenation of both strings in @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int len1 = strlen(dest);
+	int len2 = strlen(src);
+
+	for (i = len1; i <= len1 + len2; i++)
+		dest[i] = src[i - len1];
+		return (dest);
+	_putchar('\n');
+}
