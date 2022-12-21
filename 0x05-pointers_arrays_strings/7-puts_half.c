a8f5ce8885a8e02f3ac2433dd1158ce908165851
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-20 20:44:22 -0800

    7-puts_half.c commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <strinhg.h>
+/**
+ * puts_half - print the second half of the string
+ * @str: arguement to be passed
+ */
+
+void puts_half(char *str)
+{
+	int i, n;
+	int len = strlen(str);
+
+	for (len = 0; str[len] != '\0'; len++)
+	
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = ((len - 1) / 2);
+	for (i = n + 1; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
