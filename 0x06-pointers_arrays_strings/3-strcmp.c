e9600cc862c3ab4e6698228adf34b3a85df82ffb
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-23 19:42:27 -0800

    3-strcmp.c commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,9 +8,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i, j, res;
 
 	for (i = 0; s1[i] != '\0'; i++)
 		for (j = 0; s[j] != '\0'; j++)
-			return s1[i] - s2[j];
+			res = s1[i] - s2[j];
+		return (res);
 }
