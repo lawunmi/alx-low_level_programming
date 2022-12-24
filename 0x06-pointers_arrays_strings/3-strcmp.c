e827a427a487df847feac120ab196d006c5cdc9b
Mistura Raheem <olawunmiraheem@gmail.com>
2022-12-23 19:36:49 -0800

    3-strcmp.c commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: first string arguement
+ * @s2: second string arguement
+ * Return: response after comparison betweenstrings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		for (j = 0; s[j] != '\0'; j++)
+			return s1[i] - s2[j];
+}
